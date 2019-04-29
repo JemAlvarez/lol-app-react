d88a0613e8c00f0fc16b5a1786f77fe54b3989c7
Jem <jemalvarez22@gmail.com>
2019-04-29 13:08:19 -0400

    Finish (mostly) navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,59 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
+import { FaSearch, FaCaretDown, FaCog } from "react-icons/fa"
 
 const Navbar = () => {
+    const [region, setRegion] = useState('NA')
+
+    useEffect(() => {
+        if (localStorage.getItem('region')) {
+            setRegion(localStorage.getItem('region'))
+        }
+    }, [])
+    
+    const changeRegion = (region) => {
+        setRegion(region)
+        localStorage.setItem('region', region)
+    }
+
     return (
-        <nav>
-            <div>
-                <NavLink exact to="/" activeClassName="nav-link--active">Summoner</NavLink>
-                <NavLink to="/champion" activeClassName="nav-link--active">Champions</NavLink>
-                <div class="dropdown">
-                    <button class="dropbtn">Random</button>
-                    <div class="dropdown-content">
-                        <NavLink to="/random-lane" activeClassName="nav-link--active">Lane</NavLink>
-                        <NavLink to="/random-champion" activeClassName="nav-link--active">Champion</NavLink>
+        <nav className="navbar">
+            <div className="container navbar--inner">
+                <div className="navbar__container">
+                    <NavLink exact to="/" className="navbar__link" activeClassName="navbar__link--active">Summoner</NavLink>
+                    <NavLink to="/champion" className="navbar__link" activeClassName="navbar__link--active">Champions</NavLink>
+                    <div className="navbar__dropdown">
+                        <button className="navbar__link">Random <FaCaretDown /></button>
+                        <div className="navbar__dropdown-content">
+                            <NavLink to="/random-lane" className="navbar__link" activeClassName="navbar__link--active">Lane</NavLink>
+                            <NavLink to="/random-champion" className="navbar__link" activeClassName="navbar__link--active">Champion</NavLink>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div>
-                <div>
-                    <input type="text" />
-                    <select>
-                        <option>NA</option>
-                        <option>EUW</option>
-                        <option>KR</option>
-                    </select>
-                    <input type="submit" />
+                <div className="navbar__container navbar__container--custom">
+                    <div className="navbar__form">
+                        <input type="text" className="navbar__form__input" placeholder="Search summoner..." />
+                        <div className="navbar__dropdown navbar__dropdown--custom">
+                            <button className="navbar__link navbar__link--custom">{region}</button>
+                            <div className="navbar__dropdown-content navbar__dropdown-content--custom">
+                                <p className="navbar__dropdown-region" onClick={() => {changeRegion('BR')}}>BR</p>
+                                <p className="navbar__dropdown-region" onClick={() => {changeRegion('EUNE')}}>EUNE</p>
+                                <p className="navbar__dropdown-region" onClick={() => {changeRegion('EUW')}}>EUW</p>
+                                <p className="navbar__dropdown-region" onClick={() => {changeRegion('JP')}}>JP</p>
+                                <p className="navbar__dropdown-region" onClick={() => {changeRegion('KR')}}>KR</p>
+                                <p className="navbar__dropdown-region" onClick={() => {changeRegion('LAN')}}>LAN</p>
+                                <p className="navbar__dropdown-region" onClick={() => {changeRegion('LAS')}}>LAS</p>
+                                <p className="navbar__dropdown-region" onClick={() => {changeRegion('NA')}}>NA</p>
+                                <p className="navbar__dropdown-region" onClick={() => {changeRegion('OCE')}}>OCE</p>
+                                <p className="navbar__dropdown-region" onClick={() => {changeRegion('TR')}}>TR</p>
+                                <p className="navbar__dropdown-region" onClick={() => {changeRegion('RU')}}>RU</p>
+                                <p className="navbar__dropdown-region" onClick={() => {changeRegion('PBE')}}>PBE</p>
+                            </div>
+                        </div>
+                        <button className="navbar__form__submit"><FaSearch /></button>
+                    </div>
+                    <button className="navbar__menu navbar__link"><FaCog /></button>
                 </div>
-                <button>M</button>
             </div>
         </nav>
     )
